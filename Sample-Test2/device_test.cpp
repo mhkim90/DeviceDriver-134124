e9e037c61bdb42ede65236f8812a2a64e6e9d789
minho.kim <mh2022.kim@samsung.com>
2024-07-05 10:42:07 +0900

    add app test

--- Sample-Test2/device_test.cpp
+++ Sample-Test2/device_test.cpp
@@ -25,6 +25,8 @@ public:
 	const int read_val = 1;
 	const int write_val = 2;
 	const long addr = 0x00;
+	const long startAddr = 0x00;
+	const long endAddr = 0x04;
 protected:
 	void SetUp() override {
 
@@ -62,4 +64,32 @@ TEST_F(TestFixture, WriteFail) {
 	EXPECT_CALL(fmd, write(addr, write_val))
 		.Times(0);
 	EXPECT_THROW(driver.write(addr, write_val), WriteFailException);
+}
+
+TEST_F(TestFixture, AppReadAndPrint) {
+	string expected = "";
+	for (long addr_ = startAddr; addr_ < endAddr; addr_++) {
+		int val = read_val + (int)addr_;
+		expected = expected + to_string(val) + "\n";
+		EXPECT_CALL(fmd, read(addr_))
+			.Times(num_repeat)
+			.WillRepeatedly(Return(val));
+	}
+	testing::internal::CaptureStdout();
+	app.ReadAndPrint(startAddr, endAddr);
+	EXPECT_EQ(
+		testing::internal::GetCapturedStdout(), 
+		expected);
+}
+
+TEST_F(TestFixture, AppWriteAll) {
+	for (long addr_ = startAddr; addr_ < endAddr; addr_++) {
+		EXPECT_CALL(fmd, read(addr_))
+			.Times(1)
+			.WillOnce(Return(0xFF));
+		EXPECT_CALL(fmd, write(addr_, write_val))
+			.Times(1);
+	}
+	app.WriteAll(write_val);
+
 }
