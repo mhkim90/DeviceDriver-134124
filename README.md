401087c60868d2a735004e91cb23f1136d35e695
minho.kim <mh2022.kim@samsung.com>
2024-07-05 09:14:38 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## DeviceDriver
+
+장치드라이버 테스트
---
+++ Sample-Test2/device_test.cpp
@@ -0,0 +1,37 @@
+﻿#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include "../cra_0624/DeviceDriver.cpp"
+#include <string>
+#include <vector>
+#include <exception>
+#include <ctime>
+using namespace std;
+using namespace testing;
+
+class MockFMD : public FlashMemoryDevice {
+public:
+	MOCK_METHOD(unsigned char, read, (long), (override));
+	MOCK_METHOD(void, write, (long, unsigned char), (override));
+};
+
+class TestFixture : public ::testing::Test {
+public:
+	MockFMD fmd;
+	DeviceDriver driver{ &fmd };
+
+	const int num_repeat = 5;
+protected:
+	void SetUp() override {
+
+	}
+};
+
+TEST_F(TestFixture, SameResult) {
+	long addr = 0x00;
+	EXPECT_CALL(fmd, read(addr))
+		.Times(num_repeat)
+		.WillRepeatedly(Return(1));
+	for (int i = 0; i < num_repeat; i++) {
+		driver.read(addr);
+	}
+}
